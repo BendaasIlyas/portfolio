f3de0f7890708dd002e6aaccbaf47a2a591595fe
Ilyas Bendaas <ilyesben93@live.fr>
2020-07-24 11:52:39 +0100

    Define the CRUD operations

--- app/Http/Controllers/EducationController.php
+++ app/Http/Controllers/EducationController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\EducationCollection;
+use App\Education;
 
 class EducationController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+      $education = new Education([
+        'title' => $request->get('title'),
+        'start_date' => $request->get('start_date'),
+        'end_date' => $request->get('end_date'),
+        'company' => $request->get('company'),
+        'mission' => $request->get('mission')
+      ]);
+
+      $education->save();
+
+      return response()->json('success');
+    }
+
+    public function index()
+    {
+      return new EducationCollection(Education::all());
+    }
+
+    public function edit($id)
+    {
+      $post = Education::find($id);
+      return response()->json($post);
+    }
+
+    public function update($id, Request $request)
+    {
+      $post = Education::find($id);
+
+      $post->update($request->all());
+
+      return response()->json('successfully updated');
+    }
+
+    public function delete($id)
+    {
+      $post = Education::find($id);
+
+      $post->delete();
+
+      return response()->json('successfully deleted');
+    }
 }
--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\ExperienceCollection;
+use App\Experience;
 
 class ExperienceController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+      $experience = new Experience([
+        'title' => $request->get('title'),
+        'start_date' => $request->get('start_date'),
+        'end_date' => $request->get('end_date'),
+        'company' => $request->get('company'),
+        'mission' => $request->get('mission')
+      ]);
+
+      $experience->save();
+
+      return response()->json('success');
+    }
+
+    public function index()
+    {
+      return new ExperienceCollection(Experience::all());
+    }
+
+    public function edit($id)
+    {
+      $post = Experience::find($id);
+      return response()->json($post);
+    }
+
+    public function update($id, Request $request)
+    {
+      $post = Experience::find($id);
+
+      $post->update($request->all());
+
+      return response()->json('successfully updated');
+    }
+
+    public function delete($id)
+    {
+      $post = Experience::find($id);
+
+      $post->delete();
+
+      return response()->json('successfully deleted');
+    }
 }
